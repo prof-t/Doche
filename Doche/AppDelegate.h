5d3bb49f11379651b827a66367fa29eacb3c77b9
平山　亮 <hirayama16@beaa-no-MacBook-Air.local>
2015-08-28 22:25:07 +0900

    Initial Commit

---
+++ Doche/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Doche
+//
+//  Created by 平山　亮 on 2015/08/28.
+//  Copyright (c) 2015年 Ryo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Doche/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Doche
+//
+//  Created by 平山　亮 on 2015/08/28.
+//  Copyright (c) 2015年 Ryo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
